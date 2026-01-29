a1e92f16981a83adf307a9d4658d847a80653182
Koh Li Tian <e1398429@u.nus.edu>
2026-01-29 14:01:37 +0800

    init commit

---
+++ Bob.java
@@ -0,0 +1,17 @@
+public class Bob {
+    public static void main(String[] args) {
+        String logo =
+                " ______   _______   ______ \n"
+                        + "|  __  \\ |  ___  | |  __  \\ \n"
+                        + "| |__)  )| |   | | | |__)  )\n"
+                        + "|  __  ( | |   | | |  __  ( \n"
+                        + "| |__)  )| |___| | | |__)  )\n"
+                        + "|______/ |_______| |______/ \n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("___________________________");
+        System.out.println("Hello! I'm BOB\n" + "What can I do for you?");
+        System.out.println("___________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("___________________________");
+    }
+}
